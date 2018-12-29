44fce1974d2180367278b3cf6207d4b90ad23d95
Spamings <46121621+Spamings@users.noreply.github.com>
2018-12-29 14:56:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-client.user.setGame("I Hate the life.", "LISTENING");
+client.user.setGame("I Hate the life.");
    console.log(`Ready!`);
 });
 
